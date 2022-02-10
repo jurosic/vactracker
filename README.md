0ee028b3ddbcec8df66bac27d9be18baa352a3fb
Juraj <jurosik5000@gmail.com>
2022-02-10 19:31:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+vactracker
---
+++ run.py
@@ -0,0 +1,7 @@
+import threading
+from vactracker import VACTracker
+
+th = threading.Thread(target=VACTracker.checker)
+th2 = threading.Thread(target=VACTracker.console)
+th.start()
+th2.start()
